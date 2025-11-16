fc231b6116210b2cbbe8f786563ac4b15834ee98
Tamicktom <rickpokemon38@gmail.com>
2025-11-16 14:04:38 -0300

    Add .NET project configuration and update 3D test scene with new blockbench export model and transform adjustments

---
+++ src/scripts/intro_to_scripting.gd
@@ -0,0 +1,20 @@
+extends Node2D;
+
+var score: int = 0;
+
+var country_name:String = "Britania";
+var population:int = 234_123;
+var highest_altitude:float = 1_234.34;
+var land_locket:bool = false;
+
+var dict: Dictionary = {
+  "banana": "banana"
+};
+
+func _ready():
+  print(score);
+  print(country_name);
+  print(population);
+  print(highest_altitude);
+  print(land_locket);
+  print(dict);
