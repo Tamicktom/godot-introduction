922a5d3b0972f152bc25cbfabbad633b24c451cd
Tamicktom <rickpokemon38@gmail.com>
2025-11-16 19:44:33 -0300

    Refactor Player class to inherit from CharacterBody2D, add speed property, and update physics processing. Adjust player scene configuration accordingly. Introduce physics settings in project configuration to improve performance.

--- src/entities/Player.cs
+++ src/entities/Player.cs
@@ -3,8 +3,10 @@ using System;
 
 namespace Game.Entities;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
+	[Export]
+	public int Speed { get; set; } = 10;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,10 +14,13 @@ public partial class Player : Node2D
 		GD.Print("Number: " + number);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	// Called every physics frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
 	{
-		// this.Position = new Vector2(this.Position.X + 1, this.Position.Y);
-		// GD.Print("Position: " + this.Position, "Delta: " + delta);w
+		// Position = new Vector2((this.Position.X + 1) * (float)delta, this.Position.Y);
+		// GD.Print("Position: " + this.Position, "Delta: " + delta);
+
+		this.Velocity = new Vector2(1, 0) * Speed;
+		this.MoveAndSlide();
 	}
 }
