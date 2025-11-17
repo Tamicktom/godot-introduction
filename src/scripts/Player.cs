8b4ff8e5d79e46a52c924cfc89db365a35131cda
Tamicktom <rickpokemon38@gmail.com>
2025-11-17 07:55:29 -0300

    Add input configuration for player controls and link Player script in scene file

---
+++ src/scripts/Player.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+namespace Game.Scripts;
+
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector(
+			Controls.LEFT.ToString(),
+			Controls.RIGHT.ToString(),
+			Controls.UP.ToString(),
+			Controls.DOWN.ToString()
+		);
+
+		Velocity = direction * Speed;
+
+		MoveAndSlide();
+	}
+}
---
+++ src/scripts/controls.cs
@@ -0,0 +1,16 @@
+
+using System.ComponentModel;
+
+namespace Game.Scripts;
+
+enum Controls
+{
+  [Description("UP")]
+  UP,
+  [Description("DOWN")]
+  DOWN,
+  [Description("LEFT")]
+  LEFT,
+  [Description("RIGHT")]
+  RIGHT,
+}
