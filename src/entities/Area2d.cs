3dae2eb830526d6809deef0470d7eef24d69175b
Tamicktom <rickpokemon38@gmail.com>
2025-12-03 08:10:37 -0300

    Add body entry detection to Area2d and configure Player node for grouping

--- src/entities/Area2d.cs
+++ src/entities/Area2d.cs
@@ -6,10 +6,16 @@ public partial class Area2d : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.BodyEntered += OnBodyEntered;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		GD.Print("Body entered: " + body.Name);
 	}
 }
